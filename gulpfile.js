fd9b49b4ee5417a9000ae091a7481d9b89382b4c
Emanuele Padrini <ema.padrini@gmail.com>
2017-01-24 21:23:12 +0100

    Project structure finisched

--- gulpfile.js
+++ gulpfile.js
@@ -40,14 +40,16 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
 /**
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
+ var gulp = require('gulp')
+ var sass = require('gulp-sass')
+
 gulp.task('sass', function () {
-    return gulp.src('assets/css/**')
+    return gulp.src('assets/css/main.sass')
         .pipe(sass({
-            includePaths: ['scss'],
-            onError: browserSync.notify
+          includePaths: ['scss'],
+          onError: browserSync.notify
         }))
         .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('_site/css'))
         .pipe(browserSync.reload({stream:true}))
         .pipe(gulp.dest('assets/css'));
 });
